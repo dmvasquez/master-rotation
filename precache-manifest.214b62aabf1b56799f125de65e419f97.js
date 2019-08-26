74208c2359cf94344afa19dc7a21451cf96423d8
David Vasquez <davidvasquez@Davids-MBP-2.localdomain>
2019-08-26 15:20:10 -0700

    Updates

---
+++ precache-manifest.214b62aabf1b56799f125de65e419f97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa65fe065e2b224c3fe9a499767b728",
+    "url": "/master-rotation/index.html"
+  },
+  {
+    "revision": "5165c8333094ba4ce8a9",
+    "url": "/master-rotation/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e908b384213292682c4a",
+    "url": "/master-rotation/static/js/2.50271717.chunk.js"
+  },
+  {
+    "revision": "5165c8333094ba4ce8a9",
+    "url": "/master-rotation/static/js/main.10965110.chunk.js"
+  },
+  {
+    "revision": "ba0403f9e1ddeae66a62",
+    "url": "/master-rotation/static/js/runtime~main.be8f6e93.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/master-rotation/static/media/logo.5d5d9eef.svg"
+  }
+]);
